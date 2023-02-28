46c6f62da644fbbafce6a02687b7f2abcdbabe88
Mariami-N <avetianimari@gmail.com>
2023-02-28 09:46:21 -0800

    tip calculator

---
+++ main.js
@@ -0,0 +1,17 @@
+let container = document.querySelector(".container");
+let box = document.querySelector(".box");
+let first = document.querySelector(".first");
+let second = document.querySelector(".second");
+let person = document.querySelector("input");
+let zero = document.querySelector("label");
+
+person.value=" ";
+
+person.addEventListener("input", function (){
+  if (person.value === "0"){
+    zero.classList.toggle("zero2")
+    person.classList.toggle("person2")
+  }
+    }
+);
+
