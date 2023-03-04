f09c30a7b4be42813cfb804c9bf574a9aa3287ee
Mariami-N <avetianimari@gmail.com>
2023-03-04 02:53:40 -0800

    tip app

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ let tipamount = document.querySelector(".tipamount");
 let totalperson = document.querySelector(".totalperson");
 
 
-
 person.addEventListener("input", function () {
    person = document.querySelector(".person");
    zero = document.querySelector("p");
@@ -28,64 +27,103 @@ person.addEventListener("input", function () {
   }
 });
     percent5 = document.querySelector(".percent5");
-    percent5.addEventListener('click',function () { 
-     bill = document.querySelector(".bill").value;
-     person = document.querySelector(".person").value;
-     tipamount = document.querySelector(".tipamount");
-     
+    percent5.addEventListener("click",function () { 
+    bill = document.querySelector(".bill").value;
+    person = document.querySelector(".person").value;
+    tipamount = document.querySelector(".tipamount");
+    totalperson = document.querySelector(".totalperson");
+
     if(bill && person){
-       amount = (bill * 0.05) / person;
-        tipamount.innerHTML = amount;
-    } console.log(amount)
-});
+        amount = (bill * 0.05) / person;
+        amount = amount.toFixed(2);
+        tipamount.innerHTML = `$ ${amount}`; 
+        total = (bill / person) + (bill * 0.05) / person;
+        total = total.toFixed(2);
+        totalperson.innerHTML = `$ ${total}`;
+    } 
+    });
 
 
-percent10 = document.querySelector(".percent10");
-percent10.addEventListener('click',function () { 
+    percent10 = document.querySelector(".percent10");
+    percent10.addEventListener("click",function () { 
      bill = document.querySelector(".bill").value;
      person = document.querySelector(".person").value;
      tipamount = document.querySelector(".tipamount");
-     
+     totalperson = document.querySelector(".totalperson");
+
     if(bill && person){
         amount = (bill * 0.10) / person;
-        tipamount.innerHTML = amount;
-    } console.log(amount)
-});
+        amount = amount.toFixed(2);
+        tipamount.innerHTML = `$ ${amount}`
+        total = (bill / person) + (bill * 0.1) / person;
+        total = total.toFixed(2);
+        totalperson.innerHTML = `$ ${total}`;
+    }   
+}
+);
 
 percent15 = document.querySelector(".percent15");
-percent15.addEventListener('click',function () { 
+percent15.addEventListener("click",function () { 
      bill = document.querySelector(".bill").value;
      person = document.querySelector(".person").value;
      tipamount = document.querySelector(".tipamount");
      
     if(bill && person){
         amount = (bill * 0.15) / person;
-        tipamount.innerHTML = amount;
-    } console.log(amount)
+        amount = amount.toFixed(2);
+        tipamount.innerHTML = `$ ${amount}`;
+        total = (bill / person) + (bill * 0.15) / person;
+        total = total.toFixed(2);
+        totalperson.innerHTML = `$ ${total}`;
+    } 
 });
     
 
 percent25 = document.querySelector(".percent25");
-percent25.addEventListener('click',function () { 
+percent25.addEventListener("click",function () { 
      bill = document.querySelector(".bill").value;
      person = document.querySelector(".person").value;
      tipamount = document.querySelector(".tipamount");
      
     if(bill && person){
         amount = (bill * 0.25) / person;
-        tipamount.innerHTML = amount;
+        amount = amount.toFixed(2);
+        tipamount.innerHTML = `$ ${amount}`;
+        total = (bill / person) + (bill * 0.25) / person;
+        total = total.toFixed(2);
+        totalperson.innerHTML = `$ ${total}`;
     } console.log(amount)
 });
 
 
 percent50 = document.querySelector(".percent50");
-percent50.addEventListener('click',function () { 
+percent50.addEventListener("click",function () { 
      bill = document.querySelector(".bill").value;
      person = document.querySelector(".person").value;
      tipamount = document.querySelector(".tipamount");
      
     if(bill && person){
         amount = (bill * 0.50) / person;
-        tipamount.innerHTML = amount;
-    } console.log(amount)
+        amount = amount.toFixed(2);
+        tipamount.innerHTML = `$ ${amount}`;
+        total = (bill / person) + (bill * 0.50) / person;
+        total = total.toFixed(2);
+        totalperson.innerHTML = `$ ${total}`;
+    } 
 });
+
+//  custom = document.querySelector(".custom");
+//  custom.addEventListener("input", function (){
+//     if (custom.value = "0"){
+
+//         if(bill && person){
+//             amount = (bill * custom.value ) / person;
+//             amount = amount.toFixed(2);
+//             tipamount.innerHTML = `$ ${amount}`;
+//             total = (bill / person) + (bill * custom.value) / person;
+//             total = total.toFixed(2);
+//             totalperson.innerHTML = `$ ${total}`;
+//         } 
+//     }
+    
+//  })
